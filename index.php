044b262da8123970ac7e039a09fb6090e261ec2f
AlexandreGdl <guindeuilalexandre@gmail.Com>
2019-01-07 16:19:07 +0100

    lol

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php session_start(); ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/master.css">
+    <title>UrbanRide</title>
+  </head>
+  <body>
+
+
+
+
+
+
+
+
+  </body>
+</html>
