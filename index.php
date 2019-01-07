486393ff367fa755fb0125e248e7b93dc17f46a5
AlexandreGdl <guindeuilalexandre@gmail.Com>
2019-01-07 16:54:05 +0100

    Debut index.php et rename img

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php session_start(); ?>
+<?php session_start();
+
+// <img src="img/unicorn.png" alt=""> <img src="img/maxresdefault.jpg" alt=""> <img src="img/casualskater.jpg" alt=""> <img src="img/bmxcontent.jpg" alt=""> <img src="img/craigth.jpg" alt="">
+
+ ?>
 
 
 
@@ -12,11 +16,35 @@
   </head>
   <body>
 
+      <div class="header__accueil">
+        <figure class="main__info">
+
+          <figcaption>Le nouveau skate park du youtuber Vodkprod, a la RedBox. La nouvelle structure monté par le groupe de youtubers Angevins.</figcaption>
+        </figure>
+        <div class="sub__info">
+            <figure class="sub__info__block">
+
+              <figcaption>L.A Contest</figcaption>
+            </figure>
+            <figure class="sub__info__block">
+
+              <figcaption>John Walker Le nouveau jeune talent. Check sa bio il nous raconte son parcours.</figcaption>
+            </figure>
+            <figure class="sub__info__block">
+
+              <figcaption>Les nouveaux pneux</figcaption>
+            </figure>
+            <figure class="sub__info__block">
+
+              <figcaption>David Sutter</figcaption>
+            </figure>
+
+        </div>
+
+      </div>
 
 
-
-
-
+      <h3 class="actu__call">Actu Récente</h3>
 
 
   </body>
