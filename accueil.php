f766a6b66b9926c1da854f7ad4345225bc90e7d4
AlexandreGdl <guindeuilalexandre@gmail.Com>
2019-01-16 10:10:01 +0100

    Avancer 16/01

---
+++ accueil.php
@@ -0,0 +1,120 @@
+<?php session_start();
+
+// <img src="img/unicorn.png" alt=""> <img src="img/maxresdefault.jpg" alt=""> <img src="img/casualskater.jpg" alt=""> <img src="img/bmxcontent.jpg" alt=""> <img src="img/craigth.jpg" alt="">
+
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/master.css">
+  <title>UrbanRide</title>
+</head>
+<body>
+
+
+  <header class="block__header">
+    <h2 class="actu title">Actu Tendance</h2>
+    <div class="header__accueil">
+      <figure class="main__info">
+
+        <figcaption>Le nouveau skate park du youtuber Vodkprod, a la RedBox. La nouvelle structure monté par le groupe de youtubers Angevins.</figcaption>
+      </figure>
+      <div class="sub__info">
+        <figure class="sub__info__block">
+
+          <figcaption>L.A Contest</figcaption>
+        </figure>
+        <figure class="sub__info__block">
+
+          <figcaption>John Walker Le nouveau jeune talent. Check sa bio il nous raconte son parcours.</figcaption>
+        </figure>
+        <figure class="sub__info__block">
+
+          <figcaption>Les nouveaux pneux</figcaption>
+        </figure>
+        <figure class="sub__info__block">
+
+          <figcaption>David Sutter</figcaption>
+        </figure>
+
+      </div>
+
+    </div>
+  </header>
+
+
+  <h2 class="actu__news title">Actu Récente</h3>
+  <main class="block__main">
+<section class="article__wrapper">
+
+
+    <article class="article__block">
+      <img src="" alt="" class="article__preshow__img">
+      <div class="block__info">
+          <h3 class="article__preshow__title">Lorem Ipsum Dolor Sit Amet</h3>
+
+          <!-- LIMITE A 100 MOT un truc du genre -->
+          <p class="article__preshow__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nunc vel leo feugiat condimentum. Morbi id nulla non purus ultrices hendrerit sed nec lacus. Phasellus vitae vehicula ipsum, nec eleifend nisl. Donec et mollis justo, interdum tempor ex. Donec ligula neque, auctor a augue ac, finibus faucibus enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris a nulla bibendum, feugiat augue nec, cursus nisl. Morbi euismod egestas neque ac mollis. Suspendisse mi augue, ultrices hendrerit sodales ut, consequat eget erat.</p>
+      </div>
+    </div>
+  </article>
+
+  <article class="article__block">
+    <img src="" alt="" class="article__preshow__img">
+    <div class="block__info">
+        <h3 class="article__preshow__title">Lorem Ipsum Dolor Sit Amet</h3>
+
+        <!-- LIMITE A 100 MOT un truc du genre -->
+        <p class="article__preshow__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nunc vel leo feugiat condimentum. Morbi id nulla non purus ultrices hendrerit sed nec lacus. Phasellus vitae vehicula ipsum, nec eleifend nisl. Donec et mollis justo, interdum tempor ex. Donec ligula neque, auctor a augue ac, finibus faucibus enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris a nulla bibendum, feugiat augue nec, cursus nisl. Morbi euismod egestas neque ac mollis. Suspendisse mi augue, ultrices hendrerit sodales ut, consequat eget erat.</p>
+    </div>
+  </div>
+</article>
+
+<article class="article__block">
+  <img src="" alt="" class="article__preshow__img">
+  <div class="block__info">
+      <h3 class="article__preshow__title">Lorem Ipsum Dolor Sit Amet</h3>
+
+      <!-- LIMITE A 100 MOT un truc du genre -->
+      <p class="article__preshow__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nunc vel leo feugiat condimentum. Morbi id nulla non purus ultrices hendrerit sed nec lacus. Phasellus vitae vehicula ipsum, nec eleifend nisl. Donec et mollis justo, interdum tempor ex. Donec ligula neque, auctor a augue ac, finibus faucibus enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris a nulla bibendum, feugiat augue nec, cursus nisl. Morbi euismod egestas neque ac mollis. Suspendisse mi augue, ultrices hendrerit sodales ut, consequat eget erat.</p>
+  </div>
+</article>
+</section>
+
+
+
+
+<aside class="block__aside">
+  <div class="newsletter__block">
+      <p>Inscrivez vous a nos news letter</p>
+    <form class="" action="post__newsletter.php" method="post">
+      <input type="text" name="email" placeholder="Votre email">
+      <input type="submit" name="" value="GO">
+    </form>
+  </div>
+  <article class="article__aside">
+    <h3 class="article__preshow__title">Lorem Ipsum Dolor Sit Amet</h3>
+    <p class="article__preshow__description__aside">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nunc vel leo feugiat condimentum. Morbi id nulla non purus ultrices hendrerit sed nec lacus. Phasellus vitae vehicula ipsum, nec eleifend nisl.</p>
+  </article>
+  <article class="article__aside">
+    <h3 class="article__preshow__title">Lorem Ipsum Dolor Sit Amet</h3>
+    <p class="article__preshow__description__aside">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis nunc vel leo feugiat condimentum. Morbi id nulla non purus ultrices hendrerit sed nec lacus. Phasellus vitae vehicula ipsum, nec eleifend nisl.</p>
+  </article>
+</aside>
+
+</main>
+
+
+
+
+</body>
+</html>
